32c8a2e4c8b3f3ede788e23d067ce5884ae84a35
Indira Kissoondyal <indirak@alumni.princeton.edu>
2019-01-06 00:30:21 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,19 @@
 # Create your haunted_houses migration here
+
+
+class CreateHauntedHouse < ActiveRecord::Migration[5.1]
+  
+  def change
+    create_table :haunted_house do |t|
+      t.string :name
+      t.string :location
+      t.string :theme
+      t.integer :price
+      t.boolean :family_friendly
+      t.date :opening_date
+      t.date :closing_date 
+      t.text :long_descr
+      end
+  end
+  
+end 
